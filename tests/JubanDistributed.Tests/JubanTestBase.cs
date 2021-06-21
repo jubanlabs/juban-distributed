583400580fd56efb038eb416b16625aee8a64bbd
Zefei Ji <3108252+zefeiji@users.noreply.github.com>
2021-06-22 01:56:16 +0800

    changes for ci/cd

--- tests/JubanDistributed.Tests/JubanTestBase.cs
+++ tests/JubanDistributed.Tests/JubanTestBase.cs
@@ -2,6 +2,7 @@ using System;
 using Jubanlabs.JubanShared.Common.Config;
 using Jubanlabs.JubanShared.Logging;
 using JubanShared;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -15,7 +16,8 @@ namespace Jubanlabs.JubanDistributed.Tests
             var host = Host.CreateDefaultBuilder()
                 .ConfigureLogging((hostContext, loggingBuilder) => { loggingBuilder.AddConsole().AddDebug().SetMinimumLevel(LogLevel.Trace); })
                 .Build().JubanWireUp();
-            Console.WriteLine("JubanTestBase construction done");
+            
+            Console.WriteLine("JubanTestBase construction done, env: "+host.Services.GetService<IHostEnvironment>().EnvironmentName);
         }
     }
 }
